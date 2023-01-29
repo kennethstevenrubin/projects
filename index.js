5a465d4d3dfd87047e1ce5290cc89553bcfae2f5
Ubuntu <azureuser@Fred.xiih1g4qgjyeblh4mqlykaezqc.bx.internal.cloudapp.net>
2023-01-29 14:07:28 +0000

    run in https on server

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
     try {
 
         const File = require('./modules/File');
-
         const COVID19 = require('./modules/COVID19');
         const covid19 = new COVID19();
         
+        const fs = require("fs");
+        const https = require("https");
         const express = require('express');
         const app = express();
-        const port = process.env.PORT || 80;
+        const port = process.env.PORT || 443;
 
         app.use(express.json());
 
@@ -37,7 +38,14 @@
 
         app.use(express.static(require('path').join(__dirname,
             'public')));
-        app.listen(port, () => {
+
+        const options = {
+
+            key: fs.readFileSync("/etc/letsencrypt/live/kennethstevenrubin.com/privkey.pem"),
+            cert: fs.readFileSync("/etc/letsencrypt/live/kennethstevenrubin.com/fullchain.pem"),
+        };
+        https.createServer(options, 
+            app).listen(port, () => {
 
             console.log(`Listening on: ${port}!`);
         });
